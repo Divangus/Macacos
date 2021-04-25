fe5bdba6fcfcd437de5b0b627333e26242e564ef
Marina Albalà <70697960+Vizalt@users.noreply.github.com>
2021-04-25 18:29:16 +0200

    Update ModulePlayer.h

--- Project_7_Solution/Source/ModulePlayer.h
+++ Project_7_Solution/Source/ModulePlayer.h
@@ -39,8 +39,6 @@ public:
 	// The speed in which we move the player (pixels per frame)
 	int speed = 1;
 
-	int LPosition = 0;
-
 	// The player spritesheet loaded into an SDL_Texture
 	SDL_Texture* texture = nullptr;
 	
