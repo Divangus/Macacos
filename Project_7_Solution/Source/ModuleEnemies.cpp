f0708d196ef7023d7218811359c17e728250e929
David <79161120+Divangus@users.noreply.github.com>
2021-04-27 12:43:12 +0200

    Revert "Update ModuleEnemies.cpp"
    This reverts commit f1bcdd592dc7c468887b37ac86f3b67d32050739.

--- Project_7_Solution/Source/ModuleEnemies.cpp
+++ Project_7_Solution/Source/ModuleEnemies.cpp
@@ -13,7 +13,7 @@
 #define SPAWN_MARGIN 50
 
 
-ModuleEnemies::ModuleEnemies(bool startEnabled) : Module(startEnabled)
+ModuleEnemies::ModuleEnemies()
 {
 	for(uint i = 0; i < MAX_ENEMIES; ++i)
 		enemies[i] = nullptr;
