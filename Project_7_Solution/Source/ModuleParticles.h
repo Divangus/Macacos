5506573c71b93e450904b06b366d1becd47b210a
David <79161120+Divangus@users.noreply.github.com>
2021-04-27 12:42:58 +0200

    Revert "Update ModuleParticles.h"
    This reverts commit 578ab99d715a4e745a51a3c995ed76b65ba17365.

--- Project_7_Solution/Source/ModuleParticles.h
+++ Project_7_Solution/Source/ModuleParticles.h
@@ -17,7 +17,7 @@ class ModuleParticles : public Module
 public:
 	// Constructor
 	// Initializes all the particles in the array to nullptr
-	ModuleParticles(bool startEnabled);
+	ModuleParticles();
 
 	//Destructor
 	~ModuleParticles();
