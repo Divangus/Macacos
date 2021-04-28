584bcc27a772f46d7a8217df5d5828a985b26ecd
Marina Albalà <70697960+Vizalt@users.noreply.github.com>
2021-04-28 19:47:03 +0200

    si

--- Project_7_Solution/Source/ModulePlayer.h
+++ Project_7_Solution/Source/ModulePlayer.h
@@ -69,6 +69,7 @@ private:
 
 	// The player's collider
 	Collider* collider = nullptr;
+	Collider* colliderAttack = nullptr;
 
 	// A flag to detect when the player has been destroyed
 	bool destroyed = false;
