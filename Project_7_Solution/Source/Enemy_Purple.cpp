d019a0238bd1d9e5af90c10eceb4ebdf4f341ce6
David <79161120+Divangus@users.noreply.github.com>
2021-04-28 20:34:09 +0200

    canvi sprite purple

--- Project_7_Solution/Source/Enemy_Purple.cpp
+++ Project_7_Solution/Source/Enemy_Purple.cpp
@@ -6,29 +6,28 @@
 Enemy_Purple::Enemy_Purple(int x, int y) : Enemy(x, y)
 {
 	
-	front.PushBack({ 630, 20, 41, 70 });
-	front.PushBack({ 540, 20, 45, 70 });
-	front.PushBack({ 450, 20, 40, 70 });
-	front.PushBack({ 360, 20, 27, 70 });
-	front.PushBack({ 270, 20, 39, 70 });
-	front.PushBack({ 180, 20, 48, 70 });
-	front.PushBack({ 90, 20, 38, 70 });
-	front.PushBack({ 0, 20, 36, 70 });
+	front.PushBack({ 1032, 1890, 86, 90 });
+	front.PushBack({ 946, 1890, 86, 90 });
+	front.PushBack({ 860, 1890, 86, 90 });
+	front.PushBack({ 774, 1890, 86, 90 });
+	front.PushBack({ 688, 1890, 86, 90 });
+	front.PushBack({ 602, 1890, 86, 90 });
+	front.PushBack({ 516, 1890, 86, 90 });
+	front.PushBack({ 430, 1890, 86, 90 });
 	front.speed = 0.1f;
 
-	/*stop.PushBack({ 351, 384, 47, 71 });
-	stop.speed = 0.0f;*/
-
-	back.PushBack({ 352, 1044, 42, 70 });
-	back.PushBack({ 438, 1044, 46, 70 });
-	back.PushBack({ 534, 1044, 40, 70 });
-	back.PushBack({ 636, 1044, 28, 70 });
-	back.PushBack({ 714, 1044, 40, 70 });
-	back.PushBack({ 795, 1044, 49, 70 });
-	back.PushBack({ 895, 1044, 39, 70 });
-	back.PushBack({ 988, 1044, 36, 70 });
+	stop.PushBack({ 1032,2340,86,90 });
+	stop.speed = 0.0f;
+	
+	back.PushBack({ 0, 453, 86, 90 });
+	back.PushBack({ 86, 453, 86, 90 });
+	back.PushBack({ 172, 453, 86, 90 });
+	back.PushBack({ 258, 453, 86, 90 });
+	back.PushBack({ 344, 453, 86, 90 });
+	back.PushBack({ 430, 453, 86, 90 });
+	back.PushBack({ 516, 453, 86, 90 });
+	back.PushBack({ 602, 453, 86, 90 });
 	back.speed = 0.1f;
-
 
 	/*front_hit.PushBack({ 0, 1583, 75, 75 });
 	front_hit.PushBack({ 0, 1583, 75, 75 });
@@ -41,7 +40,7 @@ Enemy_Purple::Enemy_Purple(int x, int y) : Enemy(x, y)
 	front_hit.PushBack({ 0, 1583, 75, 75 });
 	front_hit.speed = 0.1f;*/
 
-	front_hit_ground.PushBack({ 972, 1678, 90, 90 });
+	/*front_hit_ground.PushBack({ 972, 1678, 90, 90 });
 	front_hit_ground.PushBack({ 873, 1678, 90, 90 });
 	front_hit_ground.PushBack({ 776, 1678, 90, 90 });
 	front_hit_ground.PushBack({ 687, 1678, 90, 90 });
@@ -73,25 +72,25 @@ Enemy_Purple::Enemy_Purple(int x, int y) : Enemy(x, y)
 	front_recovery_behind.PushBack({ 327, 1766, 90, 90 });
 	front_recovery_behind.PushBack({ 251, 1766, 90, 90 });
 	front_recovery_behind.PushBack({ 164, 1766, 90, 90 });
-	front_recovery_behind.speed = 0.08f;
+	front_recovery_behind.speed = 0.08f;*/
 
-	front_punch.PushBack({ 986, 1880, 38, 70 });
-	front_punch.PushBack({ 892, 1880, 42, 70 });
-	front_punch.PushBack({ 810, 1880, 34, 70 });
-	front_punch.PushBack({ 705, 1880, 49, 70 });
-	front_punch.PushBack({ 619, 1880, 45, 70 });
-	front_punch.PushBack({ 540, 1880, 34, 70 });
-	front_punch.PushBack({ 442, 1880, 42, 70 });
-	front_punch.PushBack({ 366, 1880, 31, 70 });
-	front_punch.PushBack({ 243, 1880, 61, 70 });
-	front_punch.PushBack({ 174, 1880, 40, 70 });
+	front_punch.PushBack({ 1032, 2790, 86, 90 });
+	front_punch.PushBack({ 946, 2790, 86, 90 });
+	front_punch.PushBack({ 860, 2790, 86, 90 });
+	front_punch.PushBack({ 774, 2790, 86, 90 });
+	front_punch.PushBack({ 688, 2790, 86, 90 });
+	front_punch.PushBack({ 602, 2790, 86, 90 });
+	front_punch.PushBack({ 516, 2790, 86, 90 });
+	front_punch.PushBack({ 430, 2790, 86, 90 });
+	front_punch.PushBack({ 344, 2790, 86, 90 });
+	front_punch.PushBack({ 258, 2790, 86, 90 });
 	front_punch.speed = 0.22f;
 
 	
 	path.PushBack({ -0.8f, 0.0f }, 150, &front);
-	//path.PushBack({ 0.0f, 0.0f }, 50, &front_punch);
+	path.PushBack({ 0.0f, 0.0f }, 50, &front_punch);
 	path.PushBack({ 0.8f, 0.0f }, 150, &back);
-	//path.PushBack({ 0.0f, 0.0f }, 150, &stop);
+	path.PushBack({ 0.0f, 0.0f }, 150, &stop);
 
 	/*path.PushBack({ -0.3f, 0.0f }, 150, &front);
 	path.PushBack({ 0.2f, 0.0f }, 30, &front_hit_ground);
@@ -106,7 +105,8 @@ Enemy_Purple::Enemy_Purple(int x, int y) : Enemy(x, y)
 	path.PushBack({ 0.3f, 0.0f }, 150, &back);*/
 	
 
-	collider = App->collisions->AddCollider({ 0, 0, 50, 70 }, Collider::Type::ENEMY, (Module*)App->enemies);
+	collider = App->collisions->AddCollider({0, 0, 35, 70 }, Collider::Type::ENEMY, (Module*)App->enemies);
+	
 }
 
 void Enemy_Purple::Update()
