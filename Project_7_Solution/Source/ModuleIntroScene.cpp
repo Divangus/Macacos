e16a7c9df72d65bf14841061881008bad05ca31a
T4skar <79161107+T4skar@users.noreply.github.com>
2021-04-29 19:47:08 +0200

    SI

--- Project_7_Solution/Source/ModuleIntroScene.cpp
+++ Project_7_Solution/Source/ModuleIntroScene.cpp
@@ -26,7 +26,7 @@ bool ModuleIntroScene::Start()
 
 	bool ret = true;
 
-	bgTexture = App->textures->Load("Assets/StartScreen.png");
+	bgTexture = App->textures->Load("Assets/IntroScreen.png");
 	App->audio->PlayMusic("Assets/Music/introTitle.ogg", 1.0f);
 
 	App->render->camera.x = 0;
