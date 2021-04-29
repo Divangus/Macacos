b1b2e794fbbd169523d36b11b9ad1192a71b60cb
David <davidbenages02@gmail.com>
2021-04-29 22:14:18 +0200

    Update ModuleOver.cpp

--- Project_7_Solution/Source/ModuleOver.cpp
+++ Project_7_Solution/Source/ModuleOver.cpp
@@ -9,6 +9,7 @@
 #include "ModulePlayer.h"
 #include "ModuleFadeToBlack.h"
 
+#include "SDL_mixer/include/SDL_mixer.h"
 #include "SDL/include/SDL_Scancode.h"
 
 
@@ -33,6 +34,7 @@ bool ModuleOver::Start()
 	App->player->Disable();
 	
 	bgTexture = App->textures->Load("Assets/Game_Over.png");
+	Mix_FadeOutMusic(0.0);
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
@@ -43,9 +45,9 @@ bool ModuleOver::Start()
 update_status ModuleOver::Update()
 {
 	//El següent if serveix per a reiniciar el joc des del game over però falta fer un reset a tots els elements del joc
-	//if (App->input->keys[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_DOWN)
+	if (App->input->keys[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_DOWN)
 	{
-		//App->fade->FadeToBlack(this, (Module*)App->title, 90);
+		App->fade->FadeToBlack(this, (Module*)App->title, 90);
 	}
 	return update_status::UPDATE_CONTINUE;
 }
