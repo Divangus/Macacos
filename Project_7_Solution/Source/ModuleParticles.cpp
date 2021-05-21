f9d47deab35837470cd30b689715cd1ee9ac0172
Marina Albalà <70697960+Vizalt@users.noreply.github.com>
2021-05-21 09:32:30 +0200

    ups

--- Project_7_Solution/Source/ModuleParticles.cpp
+++ Project_7_Solution/Source/ModuleParticles.cpp
@@ -10,6 +10,17 @@
 
 ModuleParticles::ModuleParticles(bool startEnabled) : Module(startEnabled)
 {
+	Fire.PushBack({ 25, 1, 308, 67 });
+	Fire.PushBack({ 25,70,308,67 });
+	Fire.PushBack({ 28,138,308,67 });
+	Fire.PushBack({ 343,4,308,67 });
+	Fire.PushBack({ 343,72,308,67 });
+	Fire.PushBack({ 343,137,308,67 });
+	Fire.PushBack({ 659,1,308,67 });
+	Fire.PushBack({ 659,71,308,67 });
+	//Fire.loop = true;
+	Fire.speed = 0.1f;
+
 	for(uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
 		particles[i] = nullptr;
 }
@@ -23,6 +34,7 @@ bool ModuleParticles::Start()
 {
 	LOG("Loading particles");
 	texture = App->textures->Load("Assets/particles.png");
+	Fire_Texture = App->textures->Load("Assets/frontFire.png");
 
 	// Explosion particle
 	explosion.anim.PushBack({274, 296, 33, 30});
@@ -33,6 +45,7 @@ bool ModuleParticles::Start()
 	explosion.anim.PushBack({457, 296, 33, 30});
 	explosion.anim.loop = false;
 	explosion.anim.speed = 0.3f;
+
 
 	return true;
 }
@@ -50,6 +63,7 @@ bool ModuleParticles::CleanUp()
 			particles[i] = nullptr;
 		}
 	}
+	App->textures->Unload(Fire_Texture);
 
 	return true;
 }
@@ -70,6 +84,7 @@ void ModuleParticles::OnCollision(Collider* c1, Collider* c2)
 
 update_status ModuleParticles::Update()
 {
+	Fire.Update();
 	for(uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
 	{
 		Particle* particle = particles[i];
@@ -100,6 +115,12 @@ update_status ModuleParticles::PostUpdate()
 		}
 	}
 
+	App->render->Blit(Fire_Texture, -5, 160, &(Fire.GetCurrentFrame()), 1);
+	App->render->Blit(Fire_Texture, 250, 160, &(Fire.GetCurrentFrame()), 1);
+	App->render->Blit(Fire_Texture, 504, 160, &(Fire.GetCurrentFrame()), 1);
+	App->render->Blit(Fire_Texture, 760, 160, &(Fire.GetCurrentFrame()), 1);
+	App->render->Blit(Fire_Texture, 1018, 160, &(Fire.GetCurrentFrame()), 1);
+	App->render->Blit(Fire_Texture, 1065, 160, &(Fire.GetCurrentFrame()), 1);
 	return update_status::UPDATE_CONTINUE;
 }
 
